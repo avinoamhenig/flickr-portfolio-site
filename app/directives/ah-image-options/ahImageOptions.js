9da40dbc85826eef1006ef666f69dba53f756f87
Avinoam Henig <avhenig@gmail.com>
2014-05-29 19:42:55 -0700

    new buy button and image options ui flow

--- app/directives/ah-image-options/ahImageOptions.js
+++ app/directives/ah-image-options/ahImageOptions.js
@@ -10,7 +10,26 @@ angular.module('ahImageOptions', [])
 			active: '='
 		},
 		link: function (scope, element) {
+			var $sizeOptions = element.find('.sizeOptions'),
+				$sizesDrop = element.find('.sizesDrop');
+
 			scope.user = $window.config.paypal.user;
+			scope.sizes = [];
+
+			angular.forEach($window.config.sizeOptions, function (price, size) {
+				scope.sizes.push({size: size, price:price});
+			});
+
+			scope.selectedSize = scope.sizes[0];
+
+			scope.selectSize = function (size, price) {
+				scope.selectedSize = size;
+				$sizeOptions.removeClass('active');
+			};
+
+			$sizesDrop.on('mouseover', function () {
+				$sizeOptions.addClass('active');
+			});
 		}
 	};
 }]);
